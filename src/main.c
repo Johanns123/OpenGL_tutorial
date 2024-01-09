cc254333eb371077ad6a41d8dbb48374bcfc04c7
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2024-01-09 14:58:56 -0300

    first commit

---
+++ src/main.c
@@ -0,0 +1,57 @@
+#include <GLFW/glfw3.h>
+#include <windows.h>
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glClearColor(0.0f, 0.3f, 0.0f, 1.0f);
+
+    /* Loop until the user closes the window */
+        // glColor3f(1.0f, 0.0f, 0.5f);
+        // glBegin(GL_POLYGON);
+        // glVertex3f(-0.2f, 0.8f, 0.0f);
+        // glVertex3f(-0.2f, 0.2f, 0.0f);
+        // glVertex3f(-0.8f, 0.2f, 0.0f);
+        // glVertex3f(-0.8f, 0.8f, 0.0f);
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        
+        // static char x = 0, y = 0, z = 0;
+        // glClearColor(x, y, z, 1);
+
+        glEnd();
+
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        // x++;
+        // y+=2;
+        // z+=3;
+        // Sleep(20);
+    }
+
+    glfwTerminate();
+    return 0;
+}
